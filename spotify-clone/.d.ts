d5b4922ea504660ad47f9f89eaa141855ce44e5b
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2023-08-07 06:30:29 +0200

    almost completed

---
+++ spotify-clone/.d.ts
@@ -0,0 +1,5 @@
+// use-sound.d.ts
+declare module "use-sound" {
+  const useSound: any; // Replace 'any' with the actual type
+  export default useSound;
+}
--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -3,8 +3,39 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
-const Library = () => {
-  const onClick = () => {};
+import { Song } from "@/types";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+import useOnPlay from "@/hooks/useOnPlay";
+import MediaItem from "./MediaItem";
+
+//import MediaItem from "./MediaItem";
+
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const { user, subscription } = useUser();
+  const uploadModal = useUploadModal();
+  const authModal = useAuthModal();
+  const subscribeModal = useSubscribeModal();
+
+  const onPlay = useOnPlay(songs);
+
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
+
+    return uploadModal.onOpen();
+  };
 
   return (
     <div className="flex flex-col">
@@ -16,10 +47,23 @@ const Library = () => {
         <AiOutlinePlus
           onClick={onClick}
           size={20}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          className="
+            text-neutral-400 
+            cursor-pointer 
+            hover:text-white 
+            transition
+          "
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
